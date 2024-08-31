cccb85071a5158533030e9bb56a230f62ba5a826
unknown <felipe.matheussms@gmail.com>
2024-08-30 21:28:03 -0300

    desafio 3 e 4

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -25,20 +25,40 @@ function biggerNumber(array) {
 // Escreva um código que imprima todos os elementos de um array, exceto o primeiro e o último.
 // Caso o array tenha um tamanho menor que 3 imprima a mensagem: 'Tamanho do array inválido'
 function ignoreFirstAndLastElement(array) {
-
+  
+  if (array.length < 3){
+    return console.log("Tamanho do array inválido");
+  } else{
+    for (let i = 1; i < array.length - 1; i++){
+      console.log(array[i]);     
+    } 
+   }
+  
+  
 }
+
+
 
 // Desafio 4
 // Escreva um código que inverta a ordem dos elementos de um array.
 function invertElementsArray(array) {
-  
+ let invertido = [];
+
+ for(let i = array.length - 1; i >= 0; i--){
+  invertido.push(array[i]);
+ }
+
+ return invertido;
 }
+
 
 // Desafio 5
 //  Escreva um código que recebe uma string por parâmetro e imprima a sua versão invertida.
 function invertString(string) {
-
+  
 }
+
+invertString()
 
 // Desafio 6
 // Escreva um código que receba um array de números por parâmetro e retorne
